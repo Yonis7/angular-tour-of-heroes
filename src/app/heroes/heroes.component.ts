e471952b50e53203353fd59c99ecbd580b7b6a71
Yonis7 <yonisaden7@gmail.com>
2023-08-27 18:00:14 -0600

    Expand the Tour of Heroes application to display a list of heroes. Allow users to select a hero and display the hero's details.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+
 
 @Component({
   selector: 'app-heroes',
@@ -7,8 +9,16 @@ import { Hero } from '../hero';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent {
+
+  
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
   hero : Hero = {
-    id : 1, 
+    id : 1,
     name : 'Windstorm'
   };
+
+  heroes = HEROES;
 }
