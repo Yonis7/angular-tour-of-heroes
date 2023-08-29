a65a87360157dc9d2116a8b740805e9dd3cfad2b
Yonis7 <yonisaden7@gmail.com>
2023-08-29 17:35:28 -0400

    Inject in HeroService, which uses the service to send a message Inject in MessagesComponent, which displays that message, and also displays the ID when the user clicks a hero

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,24 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
-
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  styleUrls: ['./heroes.component.css'],
 })
-export class HeroesComponent {
 
-  
+export class HeroesComponent implements OnInit {
   selectedHero?: Hero;
+
+  heroes: Hero[] = [];
+
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`)
   }
-  hero : Hero = {
-    id : 1,
-    name : 'Windstorm'
+
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm',
   };
 
-  heroes = HEROES;
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+  }
 }
